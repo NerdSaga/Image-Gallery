ebad5b37dd458e74357a46dfac653667be980dfd
Brennan Sage <brennansage01@gmail.com>
2025-05-14 17:19:08 -0700

    Added project files.

---
+++ src/index.js
@@ -0,0 +1,25 @@
+let galleryContents = ""
+let imagesFolderPath = "images/"
+
+function add(fileName)
+{
+    let result = `
+    
+    <div>
+        <img src="${imagesFolderPath + fileName}" class="gallery_image">
+    </div>
+
+    `
+    galleryContents = galleryContents + result
+}
+
+let a = await fetch("./images")
+let t = await a.text()
+console.log(t)
+
+add("earth.jpg")
+add("felt.png")
+add("happy_black_hole.png")
+add("shroom_plush.png")
+
+document.getElementById("gallery").innerHTML = galleryContents
